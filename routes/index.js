172d564bc95f05a8ca64d704c7fb093054e8c8a0
Ronni Perez <rjperez94@gmail.com>
2017-08-12 13:22:44 +1200

    Test api w/ console out

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,19 @@
 var express = require('express');
 var fs = require('fs');
 var router = express.Router();
+var github = require('octonode');
 
 // GET: /
 router.get('/', function(req, res) {
-    res.render('index/main', {
-      title: 'Home'
-    });
+	var client = github.client();
+	client.get('/users/pksunkara', {}, function (err, status, body, headers) {
+  		console.log(body); //json object
+  		res.render('index/main', {
+      		title: 'Home',
+      		body: body
+    	});
+	});
+    
 });
 
 
