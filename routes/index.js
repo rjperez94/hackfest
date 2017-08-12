0e121cf6b84cb3b958cc04957cb162fb7285fe40
Ronni Perez <rjperez94@gmail.com>
2017-08-12 16:43:20 +1200

    Fixed api
    file check in place
    integrated model with index.js

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,41 @@
 'use strict';
 var express = require('express');
+var encode = require( 'hashcode' ).hashCode;
 var fs = require('fs');
 var router = express.Router();
 var github = require('octonode');
+var fetch = require('node-fetch');
+var User = require('../model/userData');
 
 // GET: /
 router.get('/', function(req, res) {
-	var client = github.client();
-	client.get('/users/pksunkara', {}, function (err, status, body, headers) {
-  		console.log(body); //json object
-  		res.render('index/main', {
-      		title: 'Home',
-      		body: body
-    	});
+    var hash = encode().value( res.query.query ); 
+    var fname = "../data/test/"+hash+".json";
+
+    fs.exists(fname,function(exists){
+  		if (!exists) {
+  			fetch(query)
+		    .then(function(res) {
+		        return res.json();
+		    }).then(function(json) {
+		 		fs.writeFile(fname, json, function(err) {
+    				if(err) {
+        				return console.log(err);
+    				}
+    				res.render('index/main', {
+      					title: 'Home',
+      					user: new User(json)
+    				});
+				});
+		    });
+  		} else {
+  			
+  		}
 	});
+	
+
+        
     
 });
-
 
 module.exports = router;
