5db6ae95a77185072de40c915c0f14001b9da202
Dan Ko <kodani@myvuw.ac.nz>
2017-08-13 14:41:16 +1200

    null check for blog

--- model/userData.js
+++ model/userData.js
@@ -32,29 +32,36 @@ var User = function(userjson) {
 			/** STATS FOR EACH USER */
 
 User.prototype.setName = function() {
-	if(listjson.name)
-		this.name = listjson.name;
+	if(userjson.name)
+		this.name = userjson.name;
 }
 
 User.prototype.setCompany = function() {
-	if(listjson.company)
-		this.company = listjson.company;
+	if(userjson.company)
+		this.company = userjson.company;
 	else
 		this.company = "None";
 }
 
 User.prototype.setBio = function() {
-	if(listjson.bio)
-		this.bio = listjson.bio;
+	if(userjson.bio)
+		this.bio = userjson.bio;
 	else
 		this.bio = "";
 }
 
 User.prototype.setEmail = function() {
-	if(listjson.email)
-		this.email = listjson.email;
+	if(userjson.email)
+		this.email = userjson.email;
 	else
 		this.email = "";
+}
+
+User.prototype.setBlog = function() {
+	if(userjson.blog)
+		this.blog = userjson.blog;
+	else
+		this.blog = "";
 }
 
 User.prototype.getUrl = function() {
